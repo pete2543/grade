aaa92518c45d2479dd2c763107de6fd29589fd3d
kangkei suwanna <52130080+pete2543@users.noreply.github.com>
2019-09-13 23:49:00 +0700

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -1,60 +1,27 @@
 #include<iostream>
-#include<string>
 using namespace std;
-void ListStudent(string A[10], int B[10], int);
-char Calgrade(int);
 int main()
 {
-	int i,s;
-	int  B[10];
-	string A[10];
-	cout << "lnput Number of Student :";
-	cin >> s;
-	for (i = 0; i< s; i++)
+	int a[2][3];
+	for (int i = 0; i < 2; i++) {
+		for (int j = 0; j < 3; j++) {
+			cout << "lnput  " << i <<" "<< j << " :";
+			cin >> a[i][j];
+		}
+	}
+	cout << "***********Display Array***********\n";
+	for (int i = 0; i < 2; i++)
 	{
-		cout << "lnput Name :";
-		cin >> A[i];
-		cout << "lnput score :";
-		cin >> B[i];
+		for (int j = 0; j < 3; j++)
+			cout << a[i][j] << "\t";
 		cout << endl;
 	}
-	ListStudent(A, B, s);
+	cout << "**********Transpose Matrix**********\n";
+	for (int j = 0; j < 3; j++)
+	{
+		for (int i = 0; i < 2; i++)
+			cout << a[i][j] << "\t";
+		cout << endl;
+	}
 	return 0;
-}
-void  ListStudent(string Name[10], int score[10], int array)
-{
-	int i;
-	for (i = 0; i < array; i++)
-	{
-		int g = score[i];
-		cout << "Student  Name :" << Name[i]<<"\t" << "Score " << i + 1<<"=" << score[i] << "\t";
-		cout << "Your grade is ";
-		cout<< Calgrade(g);
-		cout << endl;
-	}
-}
-char Calgrade(int g)
-{
-	char grade;
-	if (g >= 80)
-	{
-		grade = 'A';
-	}
-	else if (g >= 70)
-	{
-		grade = 'B';
-	}
-	else if (g >= 60)
-	{
-		grade = 'C';
-	}
-	else if (g >= 50)
-	{
-		grade = 'D';
-	}
-	else
-	{
-		grade = 'F';
-	}
-	return grade;
 }
